ad220dbc303c64134ac2b302ba69477e446ac8ee
Nils Elveros <nils.elveros@tromb.com>
2025-11-03 14:00:13 +0100

    fix: personNummer instead of personnummer

--- src/main/java/se/fk/github/manuellregelratttillforsakring/integration/dto/OulResponse.java
+++ src/main/java/se/fk/github/manuellregelratttillforsakring/integration/dto/OulResponse.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 public interface OulResponse {
     UUID processId();
 
-    String personnummer();
+    String personNummer();
 
     boolean resultat();
 }
